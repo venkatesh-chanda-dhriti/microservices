98b0eb5f8eb1030acfd506fa1e216021badb143a
Venkatesh Chanda <venkateshchanda7@gmail.com>
2025-12-17 20:08:02 +0530

    Docker plugin added using jib and configured docker images, port details

--- loans/src/test/java/com/eazybytes/loans/LoansApplicationTests.java
+++ loans/src/test/java/com/eazybytes/loans/LoansApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CardsApplicationTests {
+class LoansApplicationTests {
 
 	@Test
 	void contextLoads() {
